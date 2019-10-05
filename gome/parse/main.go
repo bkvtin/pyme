d8ba2331dc91085d79e101f313ececef9bd1c219
Tin Tran <tin.tran@saleshood.net>
2019-10-05 14:20:40 +0700

    parse json to struct

---
+++ gome/parse/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Student struct {
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email"`
+	Age         int    `json:"age"`
+	ClassName   string `json:"class_name"`
+	AcademyName string `json:"acedemy_name"`
+}
+
+func main() {
+	s := string(`{
+		"first_name":"Tin",
+		"last_name":"Tran",
+		"email_address":"tintran@gmail.com",
+		"age":80,
+		"course_name":"golang0110",
+		"acedemy_name":"Nordic Coder"}`)
+	data := Student{}
+
+	json.Unmarshal([]byte(s), &data)
+	fmt.Printf("Operation: %s", data.FirstName)
+}
+
